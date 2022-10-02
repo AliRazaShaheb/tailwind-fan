c6edcfa4b1f5df7514d3ad1b31255e427a8b13fd
[Ali Raza Shaheb] <[arz.shaheb@gmail.com]>
2022-10-02 16:51:36 +0530

    added px to rem classes

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # 👉🏻 tailwind-fan
+
 ### tailwind css plugin for dynamic font-size, margin, padding & more
+
 ![logo_img](https://github.com/AliRazaShaheb/tailwind-fan/blob/main/tailwind-fan.png)
 
-A plugin for the use of text, margin & padding dynamically according to the screen viewport width
+A plugin for the use of
+
+1. text, margin & padding dynamically according to the screen viewport width.
+2. pixel to rem convert classes for width, height, margin & padding
 
 ## 👉🏻 Installation
 
@@ -15,80 +20,82 @@ npm install tailwind-fan
 # Using Yarn
 yarn add tailwind-fan
 ```
+
 ## 👉🏻 Configuration
+
 Then add the plugin in tailwind.config.js do the setting to override the defaults:
 
 ```js
-
 // tailwind.config.js
 module.exports = {
- content: [],
+  content: [],
   theme: {
     extend: {},
-    
+
     //  setting for tailwind-fan plugin
 
-    tailwindFan:{
-        //customization of minimum & maximum viewport / screen
-      screen:{
-          //maximum viewport/screen width
-        maxScreenSize:'', // default 44rem 
+    tailwindFan: {
+      //customization of minimum & maximum viewport / screen
+      screen: {
+        //maximum viewport/screen width
+        maxScreenSize: "", // default 44rem
 
         //minimum viewport/screen width
-        minScreenSize:'' // default 30rem
-      }
-    }
+        minScreenSize: "", // default 30rem
+      },
+      remUnit: "", // default 16px
+    },
   },
 
   plugins: [
     // add the plugin
-    require('tailwind-fan')
+    require("tailwind-fan"),
   ],
-}
+};
 ```
 
 ## 👉🏻 Usage
 
-
 ```html
-
 <section>
-    
-    <h1 class="Dtext-[3rem_2rem]">Tailwind-fan Dynamic Text</h1>
-    <!-- 
+  <h1 class="Dtext-[3rem_2rem]">Tailwind-fan Dynamic Text</h1>
+  <!-- 
       the above example work like 
       [maximum 3rem, minimum 2rem ]
     -->
-
-    
-    <h3 class="Dtext-[3rem]">Tailwind-fan Dynamic Text Auto Scaling</h3>
-    <!-- 
+  <h3 class="Dtext-[3rem]">Tailwind-fan Dynamic Text Auto Scaling</h3>
+  <!-- 
       the above example work like 
       [maximum 3rem, minimum (3rem/1.5)]
       minimum automaticaly set 66% smaller
     -->
-
-    <!-- 
-      
+  <!-- Dynamic margin example -->
+  <div classname="Dm-[2rem_1rem]">Tailwind-fan</div>
+  <!-- Dynamic padding example -->
+  <div classname="Dp-[1rem]">Tailwind-fan</div>
+  <!-- 
       - same rules for dynamic margin & padding
       - margin & padding all classes with additon of 'D' which stands for Dynamic
       - instead of 'm-[1rem] or p-[1rem]' use below :
         Dm-[1rem] or Dp-[1rem]
-
       - some Dynamic 'D' classes examples - 
-        Dtext-[]
+        Dtext-[],Dh-[],Dw-[],
         Dm-[], Dmt-[], Dmx-[], Dmy-[] ...
         Dp-[], Dpt-[], Dpx-[], Dpy-[] ...
-    
      -->
 
-
-    <!-- Dynamic margin example -->
-    <div classname='Dm-[2rem_1rem]'>Tailwind-fan</div>
-    
-    <!-- Dynamic padding example -->
-    <div classname='Dp-[1rem]'>Tailwind-fan</div>
-
+  <!-- pixel to rem -->
+  <div classname="Rh-[768px] Rw-[1080px] ">Tailwind-fan Box</div>
+  <!-- 
+      above example will convert 768px to 48rem
+      i.e. height:768px - height: 48rem
+      - same rules for width, fontSize, margin and padding.
+      - some pixel to Rem 'R' classes examples - 
+        Rtext-[],Rh-[], Rw-[],
+        Rm-[], Rmt-[], Rmx-[], Rmy-[] ...
+        Rp-[], Rpt-[], Rpx-[], Rpy-[] ...
+     -->
 </section>
 ```
+
 ![example](https://github.com/AliRazaShaheb/tailwind-fan/blob/main/tailwind_fan_example.gif)
--- index.js
+++ index.js
@@ -1,126 +1,206 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 const setting = {
-    screen:{
-        maxScreenSize : '',
-        minScreenSize : '',
+  screen: {
+    maxScreenSize: "",
+    minScreenSize: "",
+  },
+  remUnit: "16px",
+};
+
+module.exports = plugin(function ({ matchUtilities, theme }) {
+  const dynamic = (maxSize) => {
+    // min max screen size
+    let maxScrSize = theme(
+      "tailwindFan.screen.maxScreenSize",
+      setting.screen.maxScreenSize
+    );
+    let minScrSize = theme(
+      "tailwindFan.screen.minScreenSize",
+      setting.screen.minScreenSize
+    );
+
+    if (!maxScrSize && minScrSize) {
+      maxScrSize = `calc(${minScrSize}*2)`;
     }
-}
-
-module.exports = plugin(
-    function ({ matchUtilities, theme }) {
-        
-        const dynamic = (maxSize) => {
-            
-            // min max screen size
-            let maxScrSize = theme('tailwindFan.screen.maxScreenSize', setting.screen.maxScreenSize )
-            let minScrSize = theme('tailwindFan.screen.minScreenSize', setting.screen.minScreenSize )
-
-            if(!maxScrSize && minScrSize){
-                maxScrSize = `calc(${minScrSize}*2)`;
-            } 
-            if (maxScrSize && !minScrSize) {
-                minScrSize = `calc(${maxScrSize}/2)`;
-            }
-            if (!maxScrSize && !minScrSize) {
-                maxScrSize = '44rem';
-                minScrSize = '30rem'
-            }
-            
-            //min max font size
-            const [mx, mn] = maxSize.split(" ")
-            const minFsize = mn || `(${mx}/1.5)`
-            // minimum screen size
-            const minScrSizeNum = minScrSize.match(/[^a-zA-Z]/g)?.join('');
-            // maximum screen size
-            const maxScrSizeNum = maxScrSize.match(/[^a-zA-Z]/g)?.join('');
-
-            // minimum font size
-            const minFsizeNum = minFsize.match(/[^a-zA-Z]/g)?.join('');
-            // maximum font size
-            const sizeNum = mx.match(/[^a-zA-Z]/g)?.join('');
-
-
-            return `clamp(${minFsize}, calc(${minFsize} + (${sizeNum} - ${minFsizeNum}) * ((100vw - ${minScrSize}) / (${maxScrSizeNum} - ${minScrSizeNum}))),${mx})`;
-          };
-
-        matchUtilities({
-
-        // dynamic font size
-        Dtext: nmbr => ({
-            fontSize: dynamic(nmbr),
-        }),
-
-        // ------------ margin
-
-        // dynamic margin only 
-        Dm: nmbr => ({
-            margin: dynamic(nmbr),
-        }),
-
-        // dynamic marginBottom only 
-        Dmb: nmbr => ({
-            marginBottom: dynamic(nmbr),
-        }),
-        // dynamic marginTop only 
-        Dmt: nmbr => ({
-            marginTop: dynamic(nmbr),
-        }),
-        // dynamic marginLeft only 
-        Dml: nmbr => ({
-            marginLeft: dynamic(nmbr),
-        }),
-        // dynamic marginRight only 
-        Dmr: nmbr => ({
-            marginRight: dynamic(nmbr),
-        }),
-        // dynamic margin Left & Right 
-        Dmx: nmbr => ({
-            marginLeft: dynamic(nmbr),
-            marginRight: dynamic(nmbr),
-        }),
-        // dynamic margin top & bottom 
-        Dmy: nmbr => ({
-            marginTop: dynamic(nmbr),
-            marginBottom: dynamic(nmbr),
-        }),
-
-
-
-        // ------------ padding
-
-         // dynamic padding only 
-        Dp: nmbr => ({
-            padding: dynamic(nmbr),
-        }),
-
-        // dynamic paddingBottom only 
-        Dpb: nmbr => ({
-            paddingBottom: dynamic(nmbr),
-        }),
-        // dynamic paddingTop only 
-        Dpt: nmbr => ({
-            paddingTop: dynamic(nmbr),
-        }),
-        // dynamic paddingLeft only 
-        Dpl: nmbr => ({
-            paddingLeft: dynamic(nmbr),
-        }),
-        // dynamic paddingRight only 
-        Dpr: nmbr => ({
-            paddingRight: dynamic(nmbr),
-        }),
-        // dynamic padding Left & Right 
-        Dpx: nmbr => ({
-            paddingLeft: dynamic(nmbr),
-            paddingRight: dynamic(nmbr),
-        }),
-        // dynamic padding top & bottom 
-        Dpy: nmbr => ({
-            paddingTop: dynamic(nmbr),
-            paddingBottom: dynamic(nmbr),
-        }),
-
-        });
+    if (maxScrSize && !minScrSize) {
+      minScrSize = `calc(${maxScrSize}/2)`;
     }
-)
+    if (!maxScrSize && !minScrSize) {
+      maxScrSize = "44rem";
+      minScrSize = "30rem";
+    }
+
+    //min max font size
+    const [mx, mn] = maxSize.split(" ");
+    const minFsize = mn || `(${mx}/1.5)`;
+    // minimum screen size
+    const minScrSizeNum = minScrSize.match(/[^a-zA-Z]/g)?.join("");
+    // maximum screen size
+    const maxScrSizeNum = maxScrSize.match(/[^a-zA-Z]/g)?.join("");
+
+    // minimum font size
+    const minFsizeNum = minFsize.match(/[^a-zA-Z]/g)?.join("");
+    // maximum font size
+    const sizeNum = mx.match(/[^a-zA-Z]/g)?.join("");
+
+    return `clamp(${minFsize}, calc(${minFsize} + (${sizeNum} - ${minFsizeNum}) * ((100vw - ${minScrSize}) / (${maxScrSizeNum} - ${minScrSizeNum}))),${mx})`;
+  };
+
+  function px2rm(str) {
+    if (str.includes("px")) {
+      const [remUnit, ,] = theme("tailwindFan.remUnit", setting.remUnit).split(
+        "px"
+      );
+      const [dgt, ,] = str.split("px");
+      const remDgt = Number(dgt) / remUnit;
+      return `${remDgt}rem`;
+    }
+    return str;
+  }
+
+  matchUtilities({
+    // pixel to rem
+
+    // px2rm font size
+    Rtext: (str) => ({
+      fontSize: px2rm(str),
+    }),
+
+    // px2rm widht height
+    Rw: (str) => ({ width: px2rm(str) }),
+    Rh: (str) => ({ height: px2rm(str) }),
+    //px2rm margins
+    Rm: (str) => ({ margin: px2rm(str) }),
+    Rmb: (str) => ({
+      marginBottom: px2rm(str),
+    }),
+    // px2rm marginTop only
+    Rmt: (str) => ({
+      marginTop: px2rm(str),
+    }),
+    // px2rm marginLeft only
+    Rml: (str) => ({
+      marginLeft: px2rm(str),
+    }),
+    // px2rm marginRight only
+    Rmr: (str) => ({
+      marginRight: px2rm(str),
+    }),
+    // px2rm margin Left & Right
+    Rmx: (str) => ({
+      marginLeft: px2rm(str),
+      marginRight: px2rm(str),
+    }),
+    // px2rm margin top & bottom
+    Rmy: (str) => ({
+      marginTop: px2rm(str),
+      marginBottom: px2rm(str),
+    }),
+
+    //px2rm paddings
+    Rp: (str) => ({ padding: px2rm(str) }),
+
+    Rpb: (str) => ({
+      paddingBottom: px2rm(str),
+    }),
+    // px2rm paddingTop only
+    Rpt: (str) => ({
+      paddingTop: px2rm(str),
+    }),
+    // px2rm paddingLeft only
+    Rpl: (str) => ({
+      paddingLeft: px2rm(str),
+    }),
+    // px2rm paddingRight only
+    Rpr: (str) => ({
+      paddingRight: px2rm(str),
+    }),
+    // px2rm padding Left & Right
+    Rpx: (str) => ({
+      paddingLeft: px2rm(str),
+      paddingRight: px2rm(str),
+    }),
+    // px2rm padding top & bottom
+    Rpy: (str) => ({
+      paddingTop: px2rm(str),
+      paddingBottom: px2rm(str),
+    }),
+
+    // dynamic width & height
+    Dw: (nmbr) => ({ width: dynamic(nmbr) }),
+    Dh: (nmbr) => ({ height: dynamic(nmbr) }),
+    // dynamic font size
+    Dtext: (nmbr) => ({
+      fontSize: dynamic(nmbr),
+    }),
+
+    // ------------ margin
+
+    // dynamic margin only
+    Dm: (nmbr) => ({
+      margin: dynamic(nmbr),
+    }),
+
+    // dynamic marginBottom only
+    Dmb: (nmbr) => ({
+      marginBottom: dynamic(nmbr),
+    }),
+    // dynamic marginTop only
+    Dmt: (nmbr) => ({
+      marginTop: dynamic(nmbr),
+    }),
+    // dynamic marginLeft only
+    Dml: (nmbr) => ({
+      marginLeft: dynamic(nmbr),
+    }),
+    // dynamic marginRight only
+    Dmr: (nmbr) => ({
+      marginRight: dynamic(nmbr),
+    }),
+    // dynamic margin Left & Right
+    Dmx: (nmbr) => ({
+      marginLeft: dynamic(nmbr),
+      marginRight: dynamic(nmbr),
+    }),
+    // dynamic margin top & bottom
+    Dmy: (nmbr) => ({
+      marginTop: dynamic(nmbr),
+      marginBottom: dynamic(nmbr),
+    }),
+
+    // ------------ padding
+
+    // dynamic padding only
+    Dp: (nmbr) => ({
+      padding: dynamic(nmbr),
+    }),
+
+    // dynamic paddingBottom only
+    Dpb: (nmbr) => ({
+      paddingBottom: dynamic(nmbr),
+    }),
+    // dynamic paddingTop only
+    Dpt: (nmbr) => ({
+      paddingTop: dynamic(nmbr),
+    }),
+    // dynamic paddingLeft only
+    Dpl: (nmbr) => ({
+      paddingLeft: dynamic(nmbr),
+    }),
+    // dynamic paddingRight only
+    Dpr: (nmbr) => ({
+      paddingRight: dynamic(nmbr),
+    }),
+    // dynamic padding Left & Right
+    Dpx: (nmbr) => ({
+      paddingLeft: dynamic(nmbr),
+      paddingRight: dynamic(nmbr),
+    }),
+    // dynamic padding top & bottom
+    Dpy: (nmbr) => ({
+      paddingTop: dynamic(nmbr),
+      paddingBottom: dynamic(nmbr),
+    }),
+  });
+});
